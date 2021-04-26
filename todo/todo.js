8029dfd788779c7f64781e38d9d6fe28fed5df3f
Gagan Aggarwal <gaggarwal124@gmail.com>
2021-04-26 12:18:38 +0530

    frontend designing
    frontend designing

---
+++ todo/todo.js
@@ -0,0 +1,9 @@
+let title = document.getElementById("title")
+title.addEventListener("keyup", () => {
+    console.log(title.value)
+})
+
+let desc = document.getElementById("desc")
+desc.addEventListener("keyup", () => {
+    console.log(desc.value)
+})
